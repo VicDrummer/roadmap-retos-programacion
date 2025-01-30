e6e17042ae64312cc88cfcc5a6a368bdd3f32cc9
VicDrummer <vicceron145@gmail.com>
2025-01-30 17:25:08 -0600

    Create VicDrummer.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/VicDrummer.py
@@ -0,0 +1,25 @@
+# https://python.org
+
+# comentario en una línea
+
+"""
+Esto también es un comentario 
+en varias líneas
+"""
+
+'''
+esto también es un comentario
+en varias líneas
+'''
+
+first_variable = "Primer variable"
+first_variable = "número de valor de la variable"
+
+MY_CONSTANT = "MI_CONSTANTE2" # por convención
+
+first_int = 1
+first_float = 1.5
+first_bool = "false"
+first_string = "Mi cadena de texto"
+
+print("¡hola, Python!")
